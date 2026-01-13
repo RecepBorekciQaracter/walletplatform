ce61c10a6db5234db3691884cba7fd2130371581
Recep Borekci <recep.borekci@qaracter.com>
2026-01-13 15:22:20 +0300

    feat: Created wallet entity

---
+++ src/main/java/com/example/walletplatform/models/Wallet.java
@@ -0,0 +1,62 @@
+package com.example.walletplatform.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+
+@Entity
+@Table(name="wallets")
+public class Wallet {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    @Email
+    @Column(name = "owner_email", nullable = false)
+    private String ownerEmail;
+
+    @Column(name = "wallet_code", unique = true, nullable = false)
+    @Pattern(regexp = "^WLT-[A-Za-z0-9]+$")
+    private String walletCode;
+
+    @Min(0)
+    @Column(name = "balance", nullable = false)
+    private Double balance;
+
+    public Wallet() {}
+    public Wallet(String ownerEmail, String walletCode, Double balance) {
+        this.ownerEmail = ownerEmail;
+        this.walletCode = walletCode;
+        this.balance = balance;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getOwnerEmail() {
+        return ownerEmail;
+    }
+
+    public void setOwnerEmail(String ownerEmail) {
+        this.ownerEmail = ownerEmail;
+    }
+
+    public String getWalletCode() {
+        return walletCode;
+    }
+
+    public void setWalletCode(String walletCode) {
+        this.walletCode = walletCode;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+}
