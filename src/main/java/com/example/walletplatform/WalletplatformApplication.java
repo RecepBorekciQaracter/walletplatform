0e3de2a801b2888cfe3bbc0aea12edafcd59ad05
Recep Borekci <recep.borekci@qaracter.com>
2026-01-13 15:10:23 +0300

    first commit

---
+++ src/main/java/com/example/walletplatform/WalletplatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.walletplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletplatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/walletplatform/WalletplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.walletplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletplatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
