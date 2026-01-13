c62cc182b8ac63837dbdfad6fb65c324076c04a4
Recep Borekci <recep.borekci@qaracter.com>
2026-01-14 00:16:57 +0300

    fix: @Repository annotation has added to WalletRepository and OperationRepository

--- src/main/java/com/example/walletplatform/repositories/WalletRepository.java
+++ src/main/java/com/example/walletplatform/repositories/WalletRepository.java
@@ -2,6 +2,8 @@ package com.example.walletplatform.repositories;
 
 import com.example.walletplatform.entities.WalletEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface WalletRepository extends JpaRepository<WalletEntity, Long> {
 }
