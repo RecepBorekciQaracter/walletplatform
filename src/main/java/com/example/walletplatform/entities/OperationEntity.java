16187e7ef8e499fab6aa93e6c2005524c80c54ff
Recep Borekci <recep.borekci@qaracter.com>
2026-01-14 01:05:16 +0300

    feat: Replace the methods and constructors from models to entities.

--- src/main/java/com/example/walletplatform/entities/OperationEntity.java
+++ src/main/java/com/example/walletplatform/entities/OperationEntity.java
@@ -1,6 +1,8 @@
 package com.example.walletplatform.entities;
 
 import com.example.walletplatform.enums.Type;
+import com.example.walletplatform.models.Operation;
+import com.example.walletplatform.models.Wallet;
 import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
@@ -25,4 +27,61 @@ public class OperationEntity {
     private LocalDateTime timestamp;
 
     private Boolean suspicious;
+
+    public OperationEntity() {}
+
+    public OperationEntity(Type type, Double amount, Boolean suspicious) {
+        this.type = type;
+        this.amount = amount;
+        this.timestamp = LocalDateTime.now();
+        this.suspicious = suspicious;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public WalletEntity getWallet() {
+        return wallet;
+    }
+
+    public void setWallet(WalletEntity wallet) {
+        this.wallet = wallet;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public Boolean getSuspicious() {
+        return suspicious;
+    }
+
+    public void setSuspicious(Boolean suspicious) {
+        this.suspicious = suspicious;
+    }
 }
